d67441e28815a5deff55554e07b83e40dfca9a7b
greg <grandersson1991@gmail.com>
2024-05-02 13:50:05 -0400

    created data manager class
    will be used to hold data between scenes and sessions

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static DataManager Instance;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 }
